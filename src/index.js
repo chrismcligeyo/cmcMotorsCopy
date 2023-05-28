06d4a5e4f94d73cefd0090dac220caa47bf9fe82
= <mcligeyochristopher@gmail.com>
2023-05-28 07:23:09 +0300

    home secion landing page complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // import app.scss for flex styles and bootstrap
 import './assets/css/app.scss';
 
+//import Bootstrap’s JavaScript
+import 'bootstrap';
+
 //menudropw
 let initializeMeuDropdown = require("./assets/js/app");
 initializeMeuDropdown.dropdown();
